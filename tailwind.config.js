270e6c24a297eee08d9f9f55eeb88ba9167ea363
makotoeeee <mkttoyoda@gmail.com>
2021-10-05 23:14:03 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const production = !process.env.ROLLUP_WATCH; 
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.svelte',
+      './src/**/*.html',
+    ],
+    enabled: production, // disable purge in dev
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
